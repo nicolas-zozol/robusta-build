8a6c76a95bb3ca8f059c41cc1469384553303684
Nicolas Zozol <nzozol@gmail.com>
2024-11-04 18:57:49 +0100

    feat: first version of css, structured, no design

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,4 @@
+import '../styles/modern-reset.scss'
 import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
 
---
+++ src/pages/resume/nicolas.tsx
@@ -0,0 +1,7 @@
+export const NicolasResume = () => {
+  return (
+    <div className={''}>
+      <h1>Nicolas Zozol</h1>
+    </div>
+  )
+}
