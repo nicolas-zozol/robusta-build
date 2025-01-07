c4b0802fb7a5dcea405562725e55d585774def3a
Nicolas Zozol <nzozol@gmail.com>
2025-01-07 14:19:01 +0100

    feat: more portfolio items

--- src/pages/portfolio/index.tsx
+++ src/pages/portfolio/index.tsx
@@ -2,19 +2,19 @@ import * as React from 'react'
 import { ShyLink } from '../../components/theme/TLink'
 import BasicLayout from '../layout/BasicLayout'
 
-import bcgHome from './images/bcg-home.png'
-import diool from './images/diool.png'
-import docdokuPlm from './images/docdoku-plm.png'
-import eyeloDashboard from './images/eyelo-dashboard.png'
-import nautoEventDetails from './images/nauto-event-details.png'
-import nautoEventList from './images/nauto-event-list.png'
-import samDetails from './images/sam-details.png'
-import samHome from './images/sam-home.png'
-import samNewAccessRequest from './images/sam-new-access-request.png'
-import swaapHome from './images/swaap-home.png'
-import swaapPortfolio from './images/swaap-portfolio.png'
-import swaapVault from './images/swaap-vault.png'
-import { PortfolioItem } from './PortfolioItem'
+import bcgHome from '../../components/portfolio/images/bcg-home.png'
+import diool from '../../components/portfolio/images/diool.png'
+import docdokuPlm from '../../components/portfolio/images/docdoku-plm.png'
+import eyeloDashboard from '../../components/portfolio/images/eyelo-dashboard.png'
+import nautoEventDetails from '../../components/portfolio/images/nauto-event-details.png'
+import nautoEventList from '../../components/portfolio/images/nauto-event-list.png'
+import samDetails from '../../components/portfolio/images/sam-details.png'
+import samHome from '../../components/portfolio/images/sam-home.png'
+import samNewAccessRequest from '../../components/portfolio/images/sam-new-access-request.png'
+import swaapHome from '../../components/portfolio/images/swaap-home.png'
+import swaapPortfolio from '../../components/portfolio/images/swaap-portfolio.png'
+import swaapVault from '../../components/portfolio/images/swaap-vault.png'
+import { PortfolioItem } from '../../components/portfolio/PortfolioItem'
 
 export default function PortfolioIndex() {
   return (
@@ -28,7 +28,7 @@ export default function PortfolioIndex() {
           <p>
             This web3 application for Swaap Finance allow users to deposit money
             into the blockchain via a secured smart contract. The smart contract
-            will start market makin for the benefit of the user.
+            will produce market making for the benefit of the user.
           </p>
 
           <p>
@@ -83,6 +83,23 @@ export default function PortfolioIndex() {
             SAM is a tool to simplify access to Renault’s internal tools. I
             proposed and implemented an economical and innovative
             CQRS/Event-Sourcing architecture
+          </p>
+        </PortfolioItem>
+
+        <PortfolioItem images={[eyeloDashboard]} title={'Eyelo (2015)'}>
+          <p>
+            Eye.lo is a tracker for telecoms. The application allow admin users
+            to create customized dashboards for functional users – such as
+            sellers or CEO. It’s a huge Javascript app for this epoch, made with
+            plain old Angular JS.
+          </p>
+        </PortfolioItem>
+
+        <PortfolioItem images={[docdokuPlm]} title={'DocDoku (2013)'}>
+          <p>
+            Docdoku PLM is a PLM software running on Ipad, transforming Catia
+            data into Three.js structures. Very innovative, we were able to
+            build this working mvp in a few month with only 5 people.
           </p>
         </PortfolioItem>
       </main>
